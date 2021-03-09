67d9f7ce19e7102d9ea0cf407824194f9176c6a2
Jan-Micha Bodensohn <micha.bodensohn@t-online.de>
2021-03-10 00:28:19 +0100

    DFSExploration Strategy

--- aset/matching/strategies.py
+++ aset/matching/strategies.py
@@ -1,6 +1,7 @@
 """Matching strategies to match between extractions and attributes."""
 import logging
 from abc import ABC, abstractmethod
+from operator import itemgetter
 from random import choices
 
 from scipy.special import softmax
@@ -218,6 +219,148 @@ class TreeSearchExploration(BaseStrategy):
                 if distance < self.max_distance and distance < closest_distances[document]:
                     rows[document].extractions[attribute.label] = extraction
                     closest_distances[document] = distance
+
+        return rows
+
+
+class DFSExploration(BaseStrategy):
+    """DFSExploration matching strategy."""
+
+    strategy_str = "DFSExploration"
+
+    def __init__(self,
+                 max_children: int,
+                 explore_far_factor: float,
+                 max_distance: float,
+                 max_interactions: int
+                 ):
+        """
+        Initialize matching strategy.
+
+        :param max_children: number of extractions to explore in every step
+        :param explore_far_factor: extraction must be closer than previous distance / explore_far_factor to be sampled
+        :param max_distance: maximum distance to accept extractions from
+        :param max_interactions: maximum number of user interactions
+        """
+        super(DFSExploration, self).__init__()
+
+        self.max_children: int = max_children
+        self.max_distance: float = max_distance
+        self.exploration_factor: float = explore_far_factor
+        self.max_interactions: int = max_interactions
+
+    def __call__(self, documents: [Document], attributes: [Attribute]):
+        """Match extractions from the given documents to the given attributes and return a list of filled rows."""
+
+        rows = [Row(attributes) for _ in documents]
+
+        for attribute in attributes:
+            print("\n\n")
+            logger.debug("Match attribute '{}'.".format(attribute.label))
+
+            num_interactions = 0
+
+            remaining = []  # (document index, extraction, distance from already expanded nodes)
+            matching_extractions = []  # (document index, extraction)
+            queue = []  # (document index, extraction)
+
+            # initialize the distances with the distances to the attribute
+            distances_to_attribute = True
+            for document_index, document in enumerate(documents):
+                for extraction in document.extractions:
+                    distance = attribute.embedding.distance(extraction.embedding)
+                    remaining.append((document_index, extraction, distance))
+
+            while num_interactions < self.max_interactions:
+
+                # find a root
+                root_iteration = 1
+                while not queue and num_interactions < self.max_interactions:
+                    temperature = 0.01 * (root_iteration ** 2)
+                    weights = [(1 - distance) / temperature for _, _, distance in remaining]
+                    softmax_weights = softmax(weights)
+
+                    document_index, extraction, distance = choices(remaining, weights=softmax_weights)[0]
+
+                    num_interactions += 1
+                    if query_user(document_index, attribute, extraction, num_interactions):
+                        remaining = list(filter(lambda x: x[0] != document_index, remaining))
+                        matching_extractions.append((document_index, extraction))
+                        queue.append((document_index, extraction))
+                    root_iteration += 1
+
+                # if the distances are still the distances to the attribute, set them to one
+                if distances_to_attribute:
+                    remaining = [(doc, ext, 1) for doc, ext, dist in remaining]
+                    distances_to_attribute = False
+
+                # explore the tree
+                while queue and num_interactions < self.max_interactions:
+                    document_index, extraction = queue.pop(0)  # pop the first element ==> FIFO queue
+
+                    # compute the distance of the current node to all other already expanded nodes
+                    distance = 1
+                    for doc, ext in matching_extractions:
+                        if ext is not extraction:
+                            dist = extraction.embedding.distance(ext.embedding)
+                            if dist < distance:
+                                distance = dist
+
+                    if len(matching_extractions) == 1:
+                        distance = 0
+
+                    # compute the distances to the current, update distances if necessary and find possible samples
+                    samples = []
+                    new_remaining = []
+                    for doc, ext, dist in remaining:
+                        new_dist = extraction.embedding.distance(ext.embedding)
+                        if new_dist < dist:  # explore only if closer to this one than to any other one
+                            new_remaining.append((doc, ext, new_dist))
+                            if dist / self.exploration_factor > distance:  # explore farther away
+                                samples.append((doc, ext, new_dist))
+                        else:
+                            new_remaining.append((doc, ext, dist))
+                    remaining = new_remaining
+
+                    samples = sorted(samples, key=itemgetter(2))
+                    samples = samples[:self.max_children]
+
+                    # query the user about the samples
+                    new_matching = []
+                    for doc, ext, dist in samples:
+                        if num_interactions < self.max_interactions:
+                            num_interactions += 1
+                            if query_user(doc, attribute, ext, num_interactions):
+                                matching_extractions.append((doc, ext))
+                                new_matching.append((doc, ext))
+
+                    # update queue and remaining
+                    queue += new_matching
+                    docs_to_remove = set(doc for doc, _ in new_matching)
+                    if docs_to_remove:
+                        remaining = list(filter(lambda x: x[0] not in docs_to_remove, remaining))
+
+            # update the distances with the remaining stack
+            for document_index, extraction in queue:
+                new_remaining = []
+                for doc, ext, dist in remaining:
+                    new_dist = extraction.embedding.distance(ext.embedding)
+                    if new_dist < dist:
+                        new_remaining.append((doc, ext, new_dist))
+                    else:
+                        new_remaining.append((doc, ext, dist))
+                remaining = new_remaining
+
+            # match based on the calculated distances
+            closest_distances = [1] * len(rows)
+            for document_index, extraction in matching_extractions:
+                rows[document_index].extractions[attribute.label] = extraction
+                closest_distances[document_index] = 0
+
+            for document_index, extraction, distance in remaining:
+                if distance < self.max_distance and distance < closest_distances[document_index]:
+                    rows[document_index].extractions[attribute.label] = extraction
+                    closest_distances[document_index] = distance
 
         return rows
 
